2141a2871e300cdc691157d7645f9c2fb997637d
hazymen <hajihaji8282jooji@gmail.com>
2026-01-13 00:16:24 +0900

    プロトタイプとしてはほぼ完成

--- app.py
+++ app.py
@@ -1,26 +1,66 @@
 
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 import webbrowser
 import os
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+# 部屋ごとのオブジェクト管理
+room_objects = {
+    1: {},
+    2: {},
+    3: {}
+}
+
+# 部屋ごとのオブジェクトIDカウンター
+room_object_id_counter = {
+    1: 1,
+    2: 1,
+    3: 1
+}
+
+# 部屋ごとの選択状態管理
+room_object_selected_by = {
+    1: {},
+    2: {},
+    3: {}
+}
+
+# ユーザーが入室している部屋を管理
+user_room = {}
+
+# グローバルオブジェクト管理（互換性のため保持）
+objects = {}
+object_id_counter = 1
+object_selected_by = {}
+
 # オブジェクト削除イベント
 @socketio.on('delete_object')
 def handle_delete_object(data):
+    room = user_room.get(request.sid)
+    if not room:
+        return
+    
     obj_id = data['id']
-    if obj_id in objects:
-        del objects[obj_id]
+    objects_dict = room_objects[room]
+    selected_dict = room_object_selected_by[room]
+    
+    if obj_id in objects_dict:
+        del objects_dict[obj_id]
         # 選択状態も解除
-        if obj_id in object_selected_by:
-            del object_selected_by[obj_id]
-        emit('object_deleted', {'id': obj_id}, broadcast=True)
+        if obj_id in selected_dict:
+            del selected_dict[obj_id]
+        emit('object_deleted', {'id': obj_id}, to=f'room_{room}')
 
 
 @socketio.on('edit_face')
 def handle_edit_face(data):
+    room = user_room.get(request.sid)
+    if not room:
+        return
+    
     obj_id = data['id']
     indices = data.get('face_indices')
     delta = data.get('delta')
@@ -29,57 +69,120 @@ def handle_edit_face(data):
         'face_indices': indices,
         'delta': delta,
         'childMeshIndex': data.get('childMeshIndex')
-    }, broadcast=True, include_self=False)
-    
-# オブジェクト情報を管理（id, type, position など）
+    }, to=f'room_{room}', include_self=False)
 
-objects = {}
-object_id_counter = 1
-# 選択状態の管理: {オブジェクトid: 選択ユーザーのsid}
-object_selected_by = {}
-from flask import request
+# オブジェクト情報を管理（id, type, position など）
 @socketio.on('select_object')
 def handle_select_object(data):
+    room = user_room.get(request.sid)
+    if not room:
+        return
+    
     obj_id = data['id']
     sid = request.sid
+    selected_dict = room_object_selected_by[room]
+    
     # すでに他ユーザーが選択していないかチェック
-    if obj_id in object_selected_by and object_selected_by[obj_id] != sid:
+    if obj_id in selected_dict and selected_dict[obj_id] != sid:
         emit('select_result', {'id': obj_id, 'result': False})
         return
-    object_selected_by[obj_id] = sid
-    emit('object_selected', {'id': obj_id, 'sid': sid}, broadcast=True)
+    selected_dict[obj_id] = sid
+    emit('object_selected', {'id': obj_id, 'sid': sid}, to=f'room_{room}')
     emit('select_result', {'id': obj_id, 'result': True})
 
 @socketio.on('deselect_object')
 def handle_deselect_object(data):
+    room = user_room.get(request.sid)
+    if not room:
+        return
+    
     obj_id = data['id']
     sid = request.sid
-    if obj_id in object_selected_by and object_selected_by[obj_id] == sid:
-        del object_selected_by[obj_id]
-        emit('object_deselected', {'id': obj_id}, broadcast=True)
+    selected_dict = room_object_selected_by[room]
+    
+    if obj_id in selected_dict and selected_dict[obj_id] == sid:
+        del selected_dict[obj_id]
+        emit('object_deselected', {'id': obj_id}, to=f'room_{room}')
 @socketio.on('disconnect')
 def handle_disconnect():
-    # 切断時にそのユーザーが選択していたものを解除
-    sid = request.sid
-    to_remove = [oid for oid, s in object_selected_by.items() if s == sid]
-    for oid in to_remove:
-        del object_selected_by[oid]
-        emit('object_deselected', {'id': oid}, broadcast=True)
+    room = user_room.get(request.sid)
+    if room:
+        # 切断時にそのユーザーが選択していたものを解除
+        selected_dict = room_object_selected_by[room]
+        to_remove = [oid for oid, s in selected_dict.items() if s == request.sid]
+        for oid in to_remove:
+            del selected_dict[oid]
+            emit('object_deselected', {'id': oid}, to=f'room_{room}')
+        
+        # ユーザーを部屋から削除
+        del user_room[request.sid]
+        
+        # ユーザー数を通知
+        broadcast_room_users()
 
 @app.route('/')
-def index():
+def lobby():
+    return render_template('lobby.html')
+
+@app.route('/room/<int:room_number>')
+def editor(room_number):
+    if room_number not in [1, 2, 3]:
+        return 'Invalid room number', 400
     return render_template('index.html')
+
+@socketio.on('join_room')
+def handle_join_room(data):
+    room = data.get('room')
+    if room not in [1, 2, 3]:
+        return {'success': False, 'error': 'Invalid room number'}
+    
+    user_room[request.sid] = room
+    join_room(f'room_{room}')
+    
+    # 現在の全オブジェクト情報を新規クライアントに送信
+    objects_list = list(room_objects[room].values())
+    emit('init_objects', objects_list)
+    
+    # ユーザー数を通知
+    broadcast_room_users()
+    
+    return {'success': True}
 
 @socketio.on('connect')
 def handle_connect():
-    # 現在の全オブジェクト情報を新規クライアントに送信
-    emit('init_objects', list(objects.values()))
+    # ロビー画面表示時はここで特に処理不要
+    pass
+
+@socketio.on('get_room_users')
+def handle_get_room_users():
+    # 各部屋のユーザー数を計算
+    room_users = {
+        'room1': len([sid for sid, r in user_room.items() if r == 1]),
+        'room2': len([sid for sid, r in user_room.items() if r == 2]),
+        'room3': len([sid for sid, r in user_room.items() if r == 3])
+    }
+    emit('room_users', room_users)
+
+def broadcast_room_users():
+    """すべての接続クライアントに部屋のユーザー数を通知"""
+    room_users = {
+        'room1': len([sid for sid, r in user_room.items() if r == 1]),
+        'room2': len([sid for sid, r in user_room.items() if r == 2]),
+        'room3': len([sid for sid, r in user_room.items() if r == 3])
+    }
+    socketio.emit('room_users', room_users, to=None)
 
 @socketio.on('add_object')
 def handle_add_object(data):
+    room = user_room.get(request.sid)
+    if not room:
+        return
+    
     global object_id_counter
+    counter = room_object_id_counter[room]
+    
     obj = {
-        'id': object_id_counter,
+        'id': counter,
         'type': data['type'],
         'position': data['position'],
     }
@@ -96,43 +199,64 @@ def handle_add_object(data):
             obj['rotation'] = data['rotation']
         if 'scale' in data:
             obj['scale'] = data['scale']
-    objects[object_id_counter] = obj
-    object_id_counter += 1
-    emit('add_object', obj, broadcast=True)
+    
+    room_objects[room][counter] = obj
+    room_object_id_counter[room] = counter + 1
+    object_id_counter = max(object_id_counter, counter + 1)
+    
+    emit('add_object', obj, to=f'room_{room}')
 
 
 # --- 追加: 回転・スケール・全体同期イベント ---
 @socketio.on('move_object')
 def handle_move_object(data):
+    room = user_room.get(request.sid)
+    if not room:
+        return
+    
     obj_id = data['id']
-    if obj_id in objects:
-        objects[obj_id]['position'] = data['position']
+    objects_dict = room_objects[room]
+    
+    if obj_id in objects_dict:
+        objects_dict[obj_id]['position'] = data['position']
         # 既存のrotation/scaleも送る
         emit('move_object', {
-            **objects[obj_id],
-        }, broadcast=True, include_self=False)
+            **objects_dict[obj_id],
+        }, to=f'room_{room}', include_self=False)
 
 @socketio.on('rotate_object')
 def handle_rotate_object(data):
+    room = user_room.get(request.sid)
+    if not room:
+        return
+    
     obj_id = data['id']
     rotation = data['rotation']
-    if obj_id in objects:
-        objects[obj_id]['rotation'] = rotation
+    objects_dict = room_objects[room]
+    
+    if obj_id in objects_dict:
+        objects_dict[obj_id]['rotation'] = rotation
         emit('rotate_object', {
-            **objects[obj_id],
-        }, broadcast=True, include_self=False)
+            **objects_dict[obj_id],
+        }, to=f'room_{room}', include_self=False)
 
 @socketio.on('scale_object')
 def handle_scale_object(data):
+    room = user_room.get(request.sid)
+    if not room:
+        return
+    
     obj_id = data['id']
     scale = data['scale']
-    if obj_id in objects:
-        objects[obj_id]['scale'] = scale
+    objects_dict = room_objects[room]
+    
+    if obj_id in objects_dict:
+        objects_dict[obj_id]['scale'] = scale
         emit('scale_object', {
-            **objects[obj_id],
-        }, broadcast=True, include_self=False)
+            **objects_dict[obj_id],
+        }, to=f'room_{room}', include_self=False)
 
 if __name__ == '__main__':
     if os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
         webbrowser.open('http://localhost:5000/')
-    socketio.run(app, host="0.0.0.0", debug=True)
+    socketio.run(app, host="0.0.0.0", debug=False)
